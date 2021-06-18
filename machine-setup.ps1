c50ab96cf6522bfa6600f292edfa40d265e5bf1a
112RG <ryleegeorge1@gmail.com>
2021-06-18 21:50:36 +1000

    Update

--- machine-setup.ps1
+++ machine-setup.ps1
@@ -61,47 +61,59 @@ $global:machinesetupconfig = @{
         'everything'
     )
     ApplicationList = @(
-	    "Microsoft.BingFinance"
-	    "Microsoft.3DBuilder"
-	    "Microsoft.BingFinance"
-	    "Microsoft.BingNews"
-	    "Microsoft.BingSports"
-	    "Microsoft.BingWeather"
-	    "Microsoft.CommsPhone"
-	    "Microsoft.Getstarted"
-	    "Microsoft.WindowsMaps"
-	    "*MarchofEmpires*"
-	    "Microsoft.GetHelp"
-	    "Microsoft.Messaging"
-	    "*Minecraft*"
-	    "Microsoft.MicrosoftOfficeHub"
-	    "Microsoft.OneConnect"
-	    "Microsoft.WindowsPhone"
-	    "Microsoft.WindowsSoundRecorder"
-	    "*Solitaire*"
-	    "Microsoft.MicrosoftStickyNotes"
-	    "Microsoft.Office.Sway"
-	    "Microsoft.XboxApp"
-	    "Microsoft.XboxIdentityProvider"
-	    "Microsoft.ZuneMusic"
-	    "Microsoft.ZuneVideo"
-	    "Microsoft.NetworkSpeedTest"
-	    "Microsoft.FreshPaint"
-	    "Microsoft.Print3D"
-	    "*Autodesk*"
-	    "*BubbleWitch*"
-        "king.com*"
-        "G5*"
-	    "*Dell*"
-	    "*Facebook*"
-	    "*Keeper*"
-	    "*Netflix*"
-	    "*Twitter*"
-	    "*Plex*"
-	    "*.Duolingo-LearnLanguagesforFree"
-	    "*.EclipseManager"
-	    "ActiproSoftwareLLC.562882FEEB491" # Code Writer
-	    "*.AdobePhotoshopExpress"
+        "Microsoft.BingNews"
+        "Microsoft.GetHelp"
+        "Microsoft.Getstarted"
+        "Microsoft.Messaging"
+        "Microsoft.Microsoft3DViewer"
+        "Microsoft.MicrosoftOfficeHub"
+        "Microsoft.MicrosoftSolitaireCollection"
+        "Microsoft.NetworkSpeedTest"
+        "Microsoft.News"
+        "Microsoft.Office.Lens"
+        "Microsoft.Office.OneNote"
+        "Microsoft.Office.Sway"
+        "Microsoft.OneConnect"
+        "Microsoft.People"
+        "Microsoft.Print3D"
+        "Microsoft.RemoteDesktop"
+        "Microsoft.SkypeApp"
+        "Microsoft.StorePurchaseApp"
+        "Microsoft.Office.Todo.List"
+        "Microsoft.Whiteboard"
+        "Microsoft.WindowsAlarms"
+        #"Microsoft.WindowsCamera"
+        "microsoft.windowscommunicationsapps"
+        "Microsoft.WindowsFeedbackHub"
+        "Microsoft.WindowsMaps"
+        "Microsoft.WindowsSoundRecorder"
+        "Microsoft.Xbox.TCUI"
+        "Microsoft.XboxApp"
+        "Microsoft.XboxGameOverlay"
+        "Microsoft.XboxIdentityProvider"
+        "Microsoft.XboxSpeechToTextOverlay"
+        "Microsoft.ZuneMusic"
+        "Microsoft.ZuneVideo"
+
+        #Sponsored Windows 10 AppX Apps
+        #Add sponsored/featured apps to remove in the "*AppName*" format
+        "*EclipseManager*"
+        "*ActiproSoftwareLLC*"
+        "*AdobeSystemsIncorporated.AdobePhotoshopExpress*"
+        "*Duolingo-LearnLanguagesforFree*"
+        "*PandoraMediaInc*"
+        "*CandyCrush*"
+        "*BubbleWitch3Saga*"
+        "*Wunderlist*"
+        "*Flipboard*"
+        "*Twitter*"
+        "*Facebook*"
+        "*Spotify*"
+        "*Minecraft*"
+        "*Royal Revolt*"
+        "*Sway*"
+        "*Speed Test*"
+        "*Dolby*"
     )
     #WallpaperUrl = 'https://raw.githubusercontent.com/sayedihashimi/sayed-tools/master/powershell/checking-out-the-view.jpg'
 }
@@ -489,6 +501,33 @@ function GetPinToTaskbarTool{
 }
 
 # https://connect.microsoft.com/PowerShell/feedback/details/1609288/pin-to-taskbar-no-longer-working-in-windows-10
+
+function GetPinToTaskbarTool{
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$downloadUrl = 'https://github.com/sayedihashimi/sayed-tools/raw/master/contrib/PinTo10v2.exe'
+    )
+    process{
+        # see if the file has already been downloaded
+        [string]$expectedPath = (join-path $global:machinesetupconfig.MachineSetupConfigFolder 'PinTo10v2.exe')
+        if(-not (test-path $expectedPath)){
+            'Downloading PinToTaskbar from [{0}] to [{1}]' -f $downloadUrl,$expectedPath | Write-Verbose
+            # make sure the directory exists
+            EnsureFolderExists -path ([System.IO.Path]::GetDirectoryName($expectedPath)) | write-verbose
+            # download the file
+            Invoke-WebRequest -Uri $downloadUrl -OutFile $expectedPath | write-verbose
+        }
+
+        if(-not (test-path $expectedPath)){
+            $msg = 'Unable to download PinToTaskbar from [{0}] to [{1}]' -f $downloadUrl,$expectedPath
+            throw $msg 
+        }
+
+        $expectedPath
+    }
+}
+
 function PinToTaskbar{
     [cmdletbinding()]
     param(
@@ -511,31 +550,6 @@ function PinToTaskbar{
     }
 }
 
-function ConfigureTaskBar{
-    [cmdletbinding()]
-    param(
-        [Parameter(Position=0)]
-        [string]$configuretaskbarhasrunpath = (Join-Path $global:machinesetupconfig.MachineSetupConfigFolder 'configtaskbar.hasrun')
-    )
-    process{
-        if(-not (Test-Path $configuretaskbarhasrunpath)){
-            $itemstopin = @(
-                "$env:ProgramFiles\Mozilla Firefox\firefox.exe"
-                "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
-                "$env:ProgramFiles\ConEmu\ConEmu64.exe"
-            )
-
-            foreach($pi in $itemstopin){
-                if (test-path $pi) {
-                    PinToTaskbar -pathtopin $pi
-                }
-            }
-
-            CreateDummyFile -filepath $configuretaskbarhasrunpath
-        }
-    }
-}
-
 function ConfigureWindows{
     [cmdletbinding()]
     param()
@@ -543,10 +557,9 @@ function ConfigureWindows{
         RunTask @(
             #{Enable-RemoteDesktop},
             # Show hidden files, Show protected OS files, Show file extensions
-            {Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions},
-            #--- File Explorer Settings ---
             {$Global:machinesetupconfig.ApplicationList | RemoveApps},
             # will expand explorer to the actual folder you're in
+            {Remove-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme},
             {Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1},
             #adds things back in your left pane like recycle bin
             {Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1},
@@ -564,14 +577,257 @@ function ConfigureWindows{
 
             #{InstallPaintDotNet}   
         )
-
-        # TODO: update mouse pointer speed
-
-        # TODO: update mouse pointer to show when CTRL is clicked
     }
 }
 
-# http://www.getpaint.net/doc/latest/UnattendedInstallation.html
+function ConfigureExplorer{
+    $Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    Set-ItemProperty -Path $Path -Name "HideIcons" -Value 1
+    Set-ItemProperty -Path $Path -Name "HideFileExt" -Value 0
+    Set-ItemProperty -Path $Path -Name "Hidden" -Value 1
+    Set-ItemProperty -Path $Path -Name "TaskbarSmallIcons" -Value 1
+    Set-ItemProperty -Path $Path -Name "TaskbarAnimations" -Value 1
+    Set-ItemProperty -Path $Path -Name "ShowTaskViewButton" -Value 0
+}
+
+Function Protect-Privacy {
+            
+    #Disables Windows Feedback Experience
+    Write-Output "Disabling Windows Feedback Experience program"
+    $Advertising = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
+    If (Test-Path $Advertising) {
+        Set-ItemProperty $Advertising Enabled -Value 0 
+    }
+            
+    #Stops Cortana from being used as part of your Windows Search Function
+    Write-Output "Stopping Cortana from being used as part of your Windows Search Function"
+    $Search = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+    If (Test-Path $Search) {
+        Set-ItemProperty $Search AllowCortana -Value 0 
+    }
+
+    #Disables Web Search in Start Menu
+    Write-Output "Disabling Bing Search in Start Menu"
+    $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
+    If (!(Test-Path $WebSearch)) {
+        New-Item $WebSearch
+    }
+    Set-ItemProperty $WebSearch DisableWebSearch -Value 1 
+            
+    #Stops the Windows Feedback Experience from sending anonymous data
+    Write-Output "Stopping the Windows Feedback Experience program"
+    $Period = "HKCU:\Software\Microsoft\Siuf\Rules"
+    If (!(Test-Path $Period)) { 
+        New-Item $Period
+    }
+    Set-ItemProperty $Period PeriodInNanoSeconds -Value 0 
+
+    #Prevents bloatware applications from returning and removes Start Menu suggestions               
+    Write-Output "Adding Registry key to prevent bloatware apps from returning"
+    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+    $registryOEM = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+    If (!(Test-Path $registryPath)) { 
+        New-Item $registryPath
+    }
+    Set-ItemProperty $registryPath DisableWindowsConsumerFeatures -Value 1 
+
+    If (!(Test-Path $registryOEM)) {
+        New-Item $registryOEM
+    }
+    Set-ItemProperty $registryOEM  ContentDeliveryAllowed -Value 0 
+    Set-ItemProperty $registryOEM  OemPreInstalledAppsEnabled -Value 0 
+    Set-ItemProperty $registryOEM  PreInstalledAppsEnabled -Value 0 
+    Set-ItemProperty $registryOEM  PreInstalledAppsEverEnabled -Value 0 
+    Set-ItemProperty $registryOEM  SilentInstalledAppsEnabled -Value 0 
+    Set-ItemProperty $registryOEM  SystemPaneSuggestionsEnabled -Value 0          
+    
+    #Preping mixed Reality Portal for removal    
+    Write-Output "Setting Mixed Reality Portal value to 0 so that you can uninstall it in Settings"
+    $Holo = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Holographic"    
+    If (Test-Path $Holo) {
+        Set-ItemProperty $Holo  FirstRunSucceeded -Value 0 
+    }
+
+    #Disables Wi-fi Sense
+    Write-Output "Disabling Wi-Fi Sense"
+    $WifiSense1 = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting"
+    $WifiSense2 = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots"
+    $WifiSense3 = "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config"
+    If (!(Test-Path $WifiSense1)) {
+        New-Item $WifiSense1
+    }
+    Set-ItemProperty $WifiSense1  Value -Value 0 
+    If (!(Test-Path $WifiSense2)) {
+        New-Item $WifiSense2
+    }
+    Set-ItemProperty $WifiSense2  Value -Value 0 
+    Set-ItemProperty $WifiSense3  AutoConnectAllowedOEM -Value 0 
+        
+    #Disables live tiles
+    Write-Output "Disabling live tiles"
+    $Live = "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"    
+    If (!(Test-Path $Live)) {      
+        New-Item $Live
+    }
+    Set-ItemProperty $Live  NoTileApplicationNotification -Value 1 
+        
+    #Turns off Data Collection via the AllowTelemtry key by changing it to 0
+    Write-Output "Turning off Data Collection"
+    $DataCollection1 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+    $DataCollection2 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
+    $DataCollection3 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection"    
+    If (Test-Path $DataCollection1) {
+        Set-ItemProperty $DataCollection1  AllowTelemetry -Value 0 
+    }
+    If (Test-Path $DataCollection2) {
+        Set-ItemProperty $DataCollection2  AllowTelemetry -Value 0 
+    }
+    If (Test-Path $DataCollection3) {
+        Set-ItemProperty $DataCollection3  AllowTelemetry -Value 0 
+    }
+    
+    #Disabling Location Tracking
+    Write-Output "Disabling Location Tracking"
+    $SensorState = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}"
+    $LocationConfig = "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration"
+    If (!(Test-Path $SensorState)) {
+        New-Item $SensorState
+    }
+    Set-ItemProperty $SensorState SensorPermissionState -Value 0 
+    If (!(Test-Path $LocationConfig)) {
+        New-Item $LocationConfig
+    }
+    Set-ItemProperty $LocationConfig Status -Value 0 
+        
+    #Disables People icon on Taskbar
+    Write-Output "Disabling People icon on Taskbar"
+    $People = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People'
+    If (Test-Path $People) {
+        Set-ItemProperty $People -Name PeopleBand -Value 0
+    }
+        
+    #Disables scheduled tasks that are considered unnecessary 
+    Write-Output "Disabling scheduled tasks"
+    Get-ScheduledTask  XblGameSaveTaskLogon | Disable-ScheduledTask
+    Get-ScheduledTask  XblGameSaveTask | Disable-ScheduledTask
+    Get-ScheduledTask  Consolidator | Disable-ScheduledTask
+    Get-ScheduledTask  UsbCeip | Disable-ScheduledTask
+    Get-ScheduledTask  DmClient | Disable-ScheduledTask
+    Get-ScheduledTask  DmClientOnScenarioDownload | Disable-ScheduledTask
+
+    Write-Output "Stopping and disabling Diagnostics Tracking Service"
+    #Disabling the Diagnostics Tracking Service
+    Stop-Service "DiagTrack"
+    Set-Service "DiagTrack" -StartupType Disabled
+}
+
+Function DisableCortana {
+    Write-Host "Disabling Cortana"
+    $Cortana1 = "HKCU:\SOFTWARE\Microsoft\Personalization\Settings"
+    $Cortana2 = "HKCU:\SOFTWARE\Microsoft\InputPersonalization"
+    $Cortana3 = "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore"
+    If (!(Test-Path $Cortana1)) {
+        New-Item $Cortana1
+    }
+    Set-ItemProperty $Cortana1 AcceptedPrivacyPolicy -Value 0 
+    If (!(Test-Path $Cortana2)) {
+        New-Item $Cortana2
+    }
+    Set-ItemProperty $Cortana2 RestrictImplicitTextCollection -Value 1 
+    Set-ItemProperty $Cortana2 RestrictImplicitInkCollection -Value 1 
+    If (!(Test-Path $Cortana3)) {
+        New-Item $Cortana3
+    }
+    Set-ItemProperty $Cortana3 HarvestContacts -Value 0
+    
+}
+
+function ConfigureTaskBar{
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$configuretaskbarhasrunpath = (Join-Path $global:machinesetupconfig.MachineSetupConfigFolder 'configtaskbar.hasrun')
+    )
+    process{
+        $SearchIcon = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+        Set-ItemProperty $SearchIcon SearchboxTaskbarMode -Value 0
+        if(-not (Test-Path $configuretaskbarhasrunpath)){
+            $itemstopin = @(
+                "$env:ProgramFiles\Mozilla Firefox\firefox.exe"
+            )
+
+            foreach($pi in $itemstopin){
+                if (test-path $pi) {
+                    PinToTaskbar -pathtopin $pi
+                }
+            }
+
+            CreateDummyFile -filepath $configuretaskbarhasrunpath
+        }
+    }
+}
+
+Function UnpinStart {
+    # https://superuser.com/a/1442733
+    #Requires -RunAsAdministrator
+
+$START_MENU_LAYOUT = @"
+<LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">
+    <LayoutOptions StartTileGroupCellWidth="6" />
+    <DefaultLayoutOverride>
+        <StartLayoutCollection>
+            <defaultlayout:StartLayout GroupCellWidth="6" />
+        </StartLayoutCollection>
+    </DefaultLayoutOverride>
+</LayoutModificationTemplate>
+"@
+
+    $layoutFile="C:\Windows\StartMenuLayout.xml"
+
+    #Delete layout file if it already exists
+    If(Test-Path $layoutFile)
+    {
+        Remove-Item $layoutFile
+    }
+
+    #Creates the blank layout file
+    $START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
+
+    $regAliases = @("HKLM", "HKCU")
+
+    #Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
+    foreach ($regAlias in $regAliases){
+        $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+        $keyPath = $basePath + "\Explorer" 
+        IF(!(Test-Path -Path $keyPath)) { 
+            New-Item -Path $basePath -Name "Explorer"
+        }
+        Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 1
+        Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
+    }
+
+    #Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process
+    Stop-Process -name explorer
+    Start-Sleep -s 5
+    $wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESCAPE}')
+    Start-Sleep -s 5
+
+    #Enable the ability to pin items again by disabling "LockedStartLayout"
+    foreach ($regAlias in $regAliases){
+        $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+        $keyPath = $basePath + "\Explorer" 
+        Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 0
+    }
+
+    #Restart Explorer and delete the layout file
+    Stop-Process -name explorer
+
+    # Uncomment the next line to make clean start menu default for all new users
+    #Import-StartLayout -LayoutPath $layoutFile -MountPath $env:SystemDrive\
+
+    Remove-Item $layoutFile
+}
 
 
 function RunTask{
@@ -613,7 +869,7 @@ function ConfigureMachine{
 
         EnsureFolderExists $codehome
         EnsureFolderExists ($global:machinesetupconfig.MachineSetupAppsFolder)
-        InstallBaseApps
+        #InstallBaseApps
         
         RunTask @(
             #{EnsurePhotoViewerRegkeyAdded},
@@ -625,12 +881,17 @@ function ConfigureMachine{
 
             #{EnsureBaseReposCloned},
             #{LoadModules},
-            {InstallSecondaryApps}
-
-            {ConfigureWindows}
+           # {InstallSecondaryApps}
+            {ConfigureExplorer},
+            {Protect-Privacy},
+            {DisableCortana},
+            {ConfigureTaskBar},
+            {UnpinStart}
+           #{ConfigureWindows}
             #{ConfigureVisualStudio},
             #{ConfigureApps}            
         )
+        Get-Process "explorer"| Stop-Process
     }
 }
 
